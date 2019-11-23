3191ae3253dd0b97b30676c118c5516d9c84e96c
Jason Barr <jason@jasonsbarr.com>
2019-11-23 13:08:41 -0600

    Add Smurf component

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Smurf = ({ age, height, id, name }) => (
+  <div id={`smurf-${id}`}>
+    <p>
+      <span>Name:</span> {name}
+    </p>
+    <p>
+      <span>Age:</span> {age}
+    </p>
+    <p>
+      <span>Height:</span> {height}
+    </p>
+  </div>
+);
+
+export default Smurf;
